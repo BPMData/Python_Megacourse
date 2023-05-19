0194489a431ea0a5d6838ecf843664a25ccf41b3
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-05-19 12:19:29 -0400

    Start of Day 7.

---
+++ 07_01.py
@@ -0,0 +1,78 @@
+# Today's code - we're going to learn a different way to remove the newlines in the output of SHOW.
+with open("07_Save.txt", "w") as file:
+	pass
+
+from num2words import num2words
+
+while True:
+    user_decision = input("Type either Add, Show, Edit, Complete, Reset or Exit:")
+    user_decision = user_decision.strip().casefold()
+
+    savefile = open("07_Save.txt", "r")
+    todos = savefile.readlines()
+    savefile.close()
+
+    match user_decision:
+        case "add":
+            todo = input("Enter a To-Do Item:") + "\n"
+            savefile = open("07_Save.txt", "r")
+            todos = savefile.readlines()
+            savefile.close()
+
+            todos.append(todo)
+
+            savefile = open("07_Save.txt", "w")
+            savefile.writelines(todos)
+            savefile.close()
+        case "show" | "display":
+           # New code starts here. Using a for-loop
+
+           new_todos = []
+
+           for item in todos:
+               new_item = item.strip("\n")
+               new_todos.append(new_item)
+            for index, item in enumerate(new_todos):
+              # We're commenting this out cuz I did this on my own.  item = item.rstrip() #rstrip removes thenewlines introduced by add to make our savefile human readable.
+                row = f"{index + 1}: {item}"
+                print(row)
+            print(f"\nYour To-Do List is {num2words(len(todos))} items long.")
+            print()
+        case "edit":
+            number = int(input("Please enter the number of the To-Do Item you wish to edit."))
+            number = number - 1
+            # Two ways of printing a variable in a line of code. Here's way #1, old schoolway.
+            print("You selected {}.".format(todos[number]))
+            new_todo = input("Please enter the To-Do Item you wish to replace this item.")
+            todos[number] = new_todo
+            # Here's the easier way to do it, as an "f-string literal" as autorecommended by PyCharm.
+            print(f"Your new To-Do Item is {todos[number]}.")
+            savefile = open("07_Save.txt", "w")
+            savefile.writelines(todos)
+            savefile.close()
+        case "complete":
+            # dir(list)
+            # help(list.remove)
+            # Go to the end of this line of code to see more messing around
+            number = int(input("Please enter the number of the To-Do Item you wish to mark as completed."))
+            todos.pop(number - 1)
+        case "reset":
+            choice = input("Continuing with Reset will erase all items from your To-Do List,\nleaving it totally blank. Continue?\nType YES if certain.")
+            choice = choice.strip().casefold()
+
+            if choice == "yes":
+                with open("07_Save.txt", "w") as savefile:
+                    savefile.write("")
+                    savefile.close()
+                    savefile = open("07_Save.txt", "r")
+                    todos = savefile.readlines()
+                    savefile.close()
+            else:
+                print("Reset aborted.")
+        case "exit":
+            break
+        case _:
+            print("You entered an unknown command.\n"
+                  "Please type either Add, Show, Edit, Complete, Reset or Exit:")
+print("Good bye!")
+
