18514d776a1c54b577a24bb5e6a1ffcdc3165d26
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-19 22:33:11 -0400

    Just getting ready for day 20! Changing the Procfile did in fact fix the Heroku deployment, btw.

---
+++ 20_01_End_Of_Module_One.py
@@ -0,0 +1,4 @@
+# For everything related to day 19, see the project TodoWebApp
+
+# End of Beginner Python, on to Intermediate Python!!
+# https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604056#search
