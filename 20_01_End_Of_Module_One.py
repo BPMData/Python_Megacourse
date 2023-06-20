f3e309023d2f9f1ecb30a8504e68ee4e73b53e72
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-20 16:34:11 -0400

    Starting Module 2 - Gonna try a new, hopefully less confusing file directory system, and also added a bunch of files to .gitignore so that I can now try just committing the entire directory rather than specific files.

--- 20_01_End_Of_Module_One.py
+++ 20_01_End_Of_Module_One.py
@@ -1,4 +1,16 @@
 # For everything related to day 19, see the project TodoWebApp
 
+# See OneNote for today (Day 20)
+
 # End of Beginner Python, on to Intermediate Python!!
 # https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604056#search
+
+# Objects, Variables, Functions:
+# https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604066#search
+
+# Methods, Lists, Tuples, Dictionaries
+# https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604074#search
+
+# Nice quick introduction to using UNIX commands in a Terminal:
+# https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604094#search
+
---
+++ Sections/2 - Intermediate Python/Day_21_01.py
@@ -0,0 +1,9 @@
+""" You'll notice I did two things:
+
+One, start the daily files with Day_ rather than the number, so that these files can be used for importing functions is None:
+    pass
+
+Two, I'mma start using more subfolders because it's becoming a mess, lol."""
+
+# Also, I'm gonna use more multiline comment (docstring) comments cuz all the grey lines is a pain to read.
+
